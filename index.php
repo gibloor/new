b78c4ede109631544dba6ef1f2516e1cd7d24d67
gibloor <48044665+gibloor@users.noreply.github.com>
2019-05-02 20:33:53 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,2 @@
+<?
+require_once("configs/config.php");
---
+++ views/adminMenu.tpl.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pageData['title']; ?></title>
+</head>
+<body>
+    <div class="usersList">
+<?
+    echo "<div class'table'>";
+    echo "<span class='td'> Login </span><span class='td'>Название книги</span><span class='td'>Как выдана</span><span class='td'> кнопочки </span>";
+    foreach ($pageData['results'] as $key => $value) {
+        echo "<form class='tr' method='post'>
+        <span class='td'> " . $value['login'] . " </span><span class='td'> ";
+        echo '<select name=\'name\'>';
+        foreach ($pageData['resultz'] as $keys => $values) {
+            echo '<option> '.$values['name'].' </option>';
+        }
+        echo "</select>";
+        echo"</span><span class='td'><select name='typeissue'>
+        <option value='По абонименту'>По абонименту</option>
+        <option value='В читательный зал'>В читательный зал</option>
+        </select></span>
+        <span class='td'>
+        <input type='submit' name='login' value='".$value['login']."'></span></form>";
+    }
+    echo "</table>";
+?>
+<a href="catalog"> <input type="submit" name="" value="Список книг"> </a>
+<link rel="stylesheet" href="/css/adminMenuStyle.css">
+</div>
+</body>
+</html>
